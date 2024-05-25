9ea54383f8b07bb2ead6aae25291b8194fccb48a
x-info <dmsadikins@gmail.com>
2024-05-25 08:55:22 +0700

    install vue js

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue';
+
+const user = ref<String | Number>();
+
+user.value = 12345;
+
+type Color = 'green' | 'pink' | 'orange';
+
+const colors = ref<Color>('pink');
+</script>
+
+<template>
+    {{ user }}
+    {{ colors }}
+</template>
